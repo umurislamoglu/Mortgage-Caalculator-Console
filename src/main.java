415f92e3fa4fafc050d996dcc6f4fc8679b7ce03
umurislamoglu <74820307+umurislamoglu@users.noreply.github.com>
2021-04-27 18:50:07 +0300

    Second Commit

--- src/main.java
+++ src/main.java
@@ -7,19 +7,45 @@ public class main {
 	public static void main(String[] args) {
 		
 		// INPUTS
+		final byte PERCENT = 100;
+		final byte MONTH = 12;
+		int principal;
+		double interestRate;
+		int period;
 		
 		Scanner scanner = new Scanner(System.in);
 		
-		System.out.print("Principal:");
-		int principal = scanner.nextInt();
+		while(true) {
+		System.out.print("Principal ($1K-$1M):");
+		principal = scanner.nextInt();
+		if (principal >=1000 && principal <=1_000_000)
+			break;
+		System.out.println("Enter a value between $1.000 and $1.000.000");
+		}
 		
+		while(true) {
+			
 		System.out.print("Annual Interest Rate:");
-		double interestRate = scanner.nextDouble();
-		interestRate = interestRate/100/12;
+		interestRate = scanner.nextDouble();
 		
+		if (interestRate >= 1 && interestRate <= 30) {
+			interestRate =  interestRate/PERCENT/MONTH;
+			break;
+		}
+		System.out.println("Interest rate must below 30%");
+		}
+		
+		
+		
+		
+		while(true) {
 		System.out.print("Period (Years):");
-		int period = scanner.nextInt();
+		period = scanner.nextInt();
+		if (period <30) 
+			break;
+		System.out.println("Period must below 30 years");
 		
+		}
 		
 		// OUTPUT
 		
